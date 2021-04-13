1f8d12ed742f7c6f9c49515d43e54d8245b6ce31
[AlexanderIvashinin] <ivashininad@gmail.com>
2021-04-13 20:16:57 +0300

    update MyStoreApp

--- MyStore/src/main/java/ru/geekbrains/MyStoreApp.java
+++ MyStore/src/main/java/ru/geekbrains/MyStoreApp.java
@@ -3,14 +3,11 @@ package ru.geekbrains;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.geekbrains.config.AppConfig;
+import ru.geekbrains.service.CartService;
 
 public class MyStoreApp {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        CartController cartController = context.getBean("cartController", CartController.class);
-        cartController.start();
-
-
     }
 }
