db7d8c5ab76bbc7f6d8a417d417f978285c23646
[AlexanderIvashinin] <ivashininad@gmail.com>
2021-04-28 19:15:50 +0300

    update CustomerRepository

--- geek-spring-boot/geek-spring-start/src/main/java/com/geekbrains/geekspringstart/model/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.geekspringstart.model.repository;
-
-import com.geekbrains.geekspringstart.model.entity.Customer;
-
-public interface CustomerRepository {
-    Customer findById(long id);
-}
