56ae064ffa1ffe1c76edcbfef4198a990740e933
[AlexanderIvashinin] <ivashininad@gmail.com>
2021-04-28 16:14:15 +0300

    add entity class Customer

---
+++ geek-spring-boot/geek-spring-start/src/main/java/com/geekbrains/geekspringstart/model/entity/Customer.java
@@ -0,0 +1,32 @@
+package com.geekbrains.geekspringstart.model.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
