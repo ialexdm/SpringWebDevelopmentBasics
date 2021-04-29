b8fbbfbf79997c75180afb6f4efa7499c9c34bfc
[AlexanderIvashinin] <ivashininad@gmail.com>
2021-04-30 01:54:53 +0300

    update Customer Repository

--- geek-spring-boot/geek-spring-start/src/main/java/com/geekbrains/geekspringstart/model/dao/repository/CustomerRepository.java
+++ geek-spring-boot/geek-spring-start/src/main/java/com/geekbrains/geekspringstart/model/dao/repository/CustomerRepository.java
@@ -1,14 +1,10 @@
 package com.geekbrains.geekspringstart.model.dao.repository;
 
 import com.geekbrains.geekspringstart.model.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
-public interface CustomerRepository {
 
-    Customer findById(long id);
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
 
-    List<Customer> findAll();
-
-    void save(Customer customer);
 }
